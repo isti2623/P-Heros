6f75e4f31aa0a05fbd5acfd48eeeda0f61d2b77f
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-09-26 02:59:03 +0600

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="text-part">
+                <h2 className="text-center fw-bold">Complete Web Development Course with Jhankar Mahbub</h2>
+                <h5 className="text-center">Start your dream today. Conquer the world.</h5>
+                <p className="text-center text-danger fw-bold">Total Budget: 20000000</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Team/Team.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Team.css'
+const Team = (props) => {
+
+    const { img, name, profession, salary, hero } = props.hero;
+    const element = <FontAwesomeIcon icon={faShoppingCart} />
+    return (
+        <div className="col-md-4 g-4">
+            <div className="item-product container-fluid mt-3">
+                <div class="card h-100">
+                    <img src={img} class="card-img-top img-fluid w100 img-thumbnail custom" alt="..."></img>
+                    <div class="card-body">
+                        <h5 class="card-title text-center fw-bold">{name}</h5>
+                        <p className="text-center text-danger">{profession}</p>
+                        <p className="text-center">{hero}</p>
+                        <p class="card-text text-center">{salary}$</p>
+                    </div>
+
+                    <button onClick={() => props.handleAddToBag(props.hero)} type="button" class="btn btn-danger">Buy Now{element}</button>
+
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Team;
